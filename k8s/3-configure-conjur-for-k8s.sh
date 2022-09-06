e77c5d750d2114633a471ff1c4475c6284e5dfba
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-09-06 18:28:38 +0200

    code review

--- k8s/3-configure-conjur-for-k8s.sh
+++ k8s/3-configure-conjur-for-k8s.sh
@@ -17,9 +17,9 @@ conjur variable set -i conjur/authn-k8s/"$CONJUR_AUTHENTICATOR_ID"/kubernetes/se
 conjur variable set -i conjur/authn-k8s/"$CONJUR_AUTHENTICATOR_ID"/kubernetes/ca-cert -v "$K8S_CA_CERT"
 
 # Check the Service account access to K8s API
-CERT="$(conjur variable get -i conjur/authn-k8s/dev-cluster/kubernetes/ca-cert)"
-TOKEN="$(conjur variable get -i conjur/authn-k8s/dev-cluster/kubernetes/service-account-token)"
-API="$(conjur variable get -i  conjur/authn-k8s/dev-cluster/kubernetes/api-url)"
+CERT="$(conjur variable get -i conjur/authn-k8s/"$CONJUR_AUTHENTICATOR_ID"/kubernetes/ca-cert)"
+TOKEN="$(conjur variable get -i conjur/authn-k8s/"$CONJUR_AUTHENTICATOR_ID"/kubernetes/service-account-token)"
+API="$(conjur variable get -i  conjur/authn-k8s/"$CONJUR_AUTHENTICATOR_ID"/kubernetes/api-url)"
 
 echo "$CERT" > k8s.crt
 if [[ "$(curl -s --cacert k8s.crt --header "Authorization: Bearer ${TOKEN}" "$API"/healthz)" == "ok" ]]; then
