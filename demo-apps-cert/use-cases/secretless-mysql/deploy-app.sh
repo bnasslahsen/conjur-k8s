5e84fa3df4d7a80fcd2cd31a76dee6ce7e1e6f26
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-12-03 17:42:52 +0100

    code review

--- demo-apps-cert/use-cases/secretless-mysql/deploy-app.sh
+++ demo-apps-cert/use-cases/secretless-mysql/deploy-app.sh
@@ -12,10 +12,6 @@ envsubst < secretless.template.yml > secretless.yml
 $KUBE_CLI create configmap secretless-config-mysql  --from-file=secretless.yml
 rm secretless.yml
 
-
-# Service account role binding
-envsubst < service-account-role.yml | $KUBE_CLI replace --force -f -
-
 # DB DEPLOYMENT
 envsubst < db.yml | $KUBE_CLI replace --force -f -
 if ! $KUBE_CLI wait deployment "$APP_DB_NAME_MYSQL" --for condition=Available=True --timeout=120s
--- demo-apps-jwt/use-cases/secretless-postgres/deploy-app.sh
+++ demo-apps-jwt/use-cases/secretless-postgres/deploy-app.sh
@@ -15,10 +15,6 @@ envsubst < secretless.template.yml > secretless.yml
 $KUBE_CLI create configmap secretless-config --from-file=secretless.yml
 rm secretless.yml
 
-
-# Service account role binding
-envsubst < service-account-role.yml | $KUBE_CLI replace --force -f -
-
 # DB DEPLOYMENT
 envsubst < db.yml | $KUBE_CLI replace --force -f -
 if ! $KUBE_CLI wait deployment "$APP_DB_NAME_POSTGRESQL" --for condition=Available=True --timeout=120s
