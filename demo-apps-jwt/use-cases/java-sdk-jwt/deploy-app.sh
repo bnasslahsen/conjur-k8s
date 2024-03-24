80e0e133b700677f56e25c576a419facae0aa6da
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-03-24 17:26:34 +0100

    samples update

--- demo-apps-jwt/use-cases/java-sdk-jwt/deploy-app.sh
+++ demo-apps-jwt/use-cases/java-sdk-jwt/deploy-app.sh
@@ -6,7 +6,7 @@ set +a
 
 $KUBE_CLI config set-context --current --namespace="$APP_NAMESPACE"
 
-$KUBE_CLI delete secret java-sdk-credentials-jwt --ignore-not-found=true
+kubectl delete configmap conjur-connect-spring-jwt --ignore-not-found=true
 
 openssl s_client -connect "$CYBERARK_CONJUR_MASTER_HOSTNAME":"$CYBERARK_CONJUR_MASTER_PORT" \
   -showcerts </dev/null 2> /dev/null | \
@@ -20,23 +20,28 @@ else
   APPLIANCE_URL=$CYBERARK_CONJUR_APPLIANCE_URL
 fi
 
+
+kubectl delete configmap mapping-config-jwt --ignore-not-found=true
+kubectl create configmap mapping-config-jwt --from-file=conjur.properties
+
 kubectl create configmap conjur-connect-spring-jwt \
-  --from-literal SPRING_PROFILES_ACTIVE=secured-java \
+  --from-literal SPRING_PROFILES_ACTIVE=secured \
   --from-literal CONJUR_ACCOUNT="$CYBERARK_CONJUR_ACCOUNT" \
-  --from-literal CONJUR_APPLIANCE_URL="$CONJUR_APPLIANCE_URL" \
+  --from-literal CONJUR_APPLIANCE_URL="$APPLIANCE_URL" \
   --from-literal CONJUR_AUTHENTICATOR_ID="$CYBERARK_CONJUR_AUTHENTICATOR_ID"  \
   --from-literal CONJUR_JWT_TOKEN_PATH="/var/run/secrets/kubernetes.io/serviceaccount/token" \
+  --from-literal CONJUR_MAPPING_PATH="/opt/secrets/conjur/conjur.properties" \
   --from-literal LOGGING_LEVEL_COM_CYBERARK=DEBUG  \
   --from-literal SPRING_MAIN_CLOUD_PLATFORM="NONE" \
   --from-file "CONJUR_SSL_CERTIFICATE=conjur.pem" 
   
 # DEPLOYMENT
-envsubst < deployment.yml | $KUBE_CLI replace --force -f -
-if ! $KUBE_CLI wait deployment "$APP_JAVA_SDK_JWT" --for condition=Available=True --timeout=90s
+envsubst < deployment.yml | kubectl replace --force -f -
+if ! kubectl wait deployment $APP_NAME-jwt --for condition=Available=True --timeout=90s
   then exit 1
 fi
 
-$KUBE_CLI get services "$APP_JAVA_SDK_JWT"
-$KUBE_CLI get pods
+kubectl get services $APP_NAME-jwt
+kubectl get pods
 
 rm "$CYBERARK_CONJUR_SSL_CERTIFICATE"
